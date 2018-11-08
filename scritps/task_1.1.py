2ab3d89f2deb2bc8d365a5bbe661da72d4f728eb
Vikrant Fernandes <badrobot161@gmail.com>
2018-11-08 14:07:02 +0530

    Seperated subscriber functions for PID Tuning GUI

--- scritps/task_1.1.py
+++ scritps/task_1.1.py
@@ -20,7 +20,10 @@ class DroneFly():
 		rospy.Subscriber('whycon/poses', PoseArray, self.get_pose)
 
 		# To tune the drone during runtime
-		rospy.Subscriber('/pid_tuning', PidTune, self.set_pid_val)
+		rospy.Subscriber('/pid_tuning_altitude', PidTune, self.set_pid_alt)
+		rospy.Subscriber('/pid_tuning_roll', PidTune, self.set_pid_roll)
+		rospy.Subscriber('/pid_tuning_pitch', PidTune, self.set_pid_pitch)
+		rospy.Subscriber('/pid_tuning_yaw', PidTune, self.set_pid_yaw)
 		
 		self.cmd = PlutoMsg()
 
@@ -52,6 +55,11 @@ class DroneFly():
 		self.kp_pitch = 0.0
 		self.ki_pitch = 0.0
 		self.kd_pitch = 0.0
+		
+		#PID constants for Yaw
+		self.kp_yaw = 0.0
+		self.ki_yaw = 0.0
+		self.kd_yaw = 0.0
 
 		#PID constants for Throttle
 		self.kp_throt = 0.0
@@ -61,8 +69,10 @@ class DroneFly():
 		# Correction values after PID is computed
 		self.correct_roll = 0.0
 		self.correct_pitch = 0.0
+		self.correct_yaw = 0.0
 		self.correct_throt = 0.0
 
+		# Loop time for PID computation. You are free to experiment with this
 		self.last_time = 0.0
 		self.loop_time = 0.032
 
@@ -148,23 +158,38 @@ class DroneFly():
 	# def publish_plot_data(self):
 
 
-	def set_pid_val(self,pid_val):
+	def set_pid_alt(self,pid_val):
+		
+		#This is the subscriber function to get the Kp, Ki and Kd values set through the GUI for Altitude
 
-		#This is the subscriber function to get the Kp, Ki and Kd values set through the GUI
+		self.kp_throt = pid_val.Kp
+		self.ki_throt = pid_val.Ki
+		self.kd_throt = pid_val.Kd
 
-		self.kp_roll = pid_val.Kp_x
-		self.kp_pitch = pid_val.Kp_y
-		self.kp_throt = pid_val.Kp_z
+	def set_pid_roll(self,pid_val):
 
-		self.ki_roll = pid_val.Ki_x
-		self.ki_pitch = pid_val.Ki_y
-		self.ki_throt = pid_val.Ki_z
+		#This is the subscriber function to get the Kp, Ki and Kd values set through the GUI for Roll
 
-		self.kd_roll = pid_val.Kd_x
-		self.kd_pitch = pid_val.Kd_y
-		self.kd_throt = pid_val.Kd_z
+		self.kp_roll = pid_val.Kp
+		self.ki_roll = pid_val.Ki
+		self.kd_roll = pid_val.Kd
+		
+	def set_pid_pitch(self,pid_val):
 
+		#This is the subscriber function to get the Kp, Ki and Kd values set through the GUI for Pitch
 
+		self.kp_pitch = pid_val.Kp
+		self.ki_pitch = pid_val.Ki
+		self.kd_pitch = pid_val.Kd
+		
+	def set_pid_yaw(self,pid_val):
+
+		#This is the subscriber function to get the Kp, Ki and Kd values set through the GUI for Yaw
+
+		self.kp_yaw = pid_val.Kp
+		self.ki_yaw = pid_val.Ki
+		self.kd_yaw = pid_val.Kd
+		
 	def get_pose(self,pose):
 
 		#This is the subscriber function to get the whycon poses
